7b117fc8ae31ea2f82b954cc425315bc1ca2d8ea
leematthewshome <lee.matthews@qlik.com>
2018-11-18 20:28:50 +1100

    Motion detection via PIR Sensor working. Code cleaned up.

--- client/motionLoop.py
+++ client/motionLoop.py
@@ -4,11 +4,9 @@
 Sensor for detecting motion using Open CV
 Author: Lee Matthews 2018
 Open computer vision (Open CV) libraries required 
-
 Motion sensing will only run if ENVIRON['motion'] == True. ENVIRON['security'] determines whether 
 motion raises an alarm or not. If not true then detected motion might only trigger a random event 
 or conversation for example.
-
 The directory path "/static/images" must exist under the robotAI root directory
 ===============================================================================================
 """
@@ -52,7 +50,6 @@ class motionLoop(object):
         # setup variables for motion detection process
         #-------------------------------------------------
         self.detector = getConfig(config, "Motion_detector")
-        self.detectPin = getConfig(config, "Motion_detectPin")
         self.framesCheck = 10                           
         self.motionChat = getConfig(config, "Motion_motionchat")
         self.securitychat = getConfig(config, "Motion_securitychat")
@@ -63,11 +60,13 @@ class motionLoop(object):
             self.chatDelay = int(getConfig(config, "Motion_chatdelay"))
             self.delay = int(getConfig(config, "Motion_detectdelay"))
             self.min_area = int(getConfig(config, "Motion_minarea"))
+            self.detectPin = int(getConfig(config, "Motion_detectPin"))
         except:
             self.Mic.say("There is a problem with one of the configuration values, so I am using my defaults.")
             self.chatDelay = 0
             self.delay = 10
             self.min_area = 500
+            self.detectPin = 0
         # set lastChat so it triggers a chat if motion detected after startup
         self.lastChat = datetime.datetime.today() - datetime.timedelta(minutes=self.chatDelay)
         # set non to blank as we handle that below using len(self.motionChat)
@@ -105,6 +104,7 @@ class motionLoop(object):
             else:
                 time.sleep(5)
 
+                
     # Steps to perform when Security Alert Triggered
     # ============================================================================================
     def securityWarn(self, camera):
@@ -163,51 +163,80 @@ class motionLoop(object):
         self.SENSORQ.put(['brain', command])
 
         
-    # Loop to actually detect motion using the camera
+    # Work out what we need to do when motion detected
+    # ============================================================================================
+    def detectionEvent(self, lastAlert, camera):
+        # Check lastAlert to see if we need to trigger a new Alert for the motion
+        curDTime = datetime.datetime.today()
+        self.logger.debug("Motion detected at %s " % curDTime)
+        diff = curDTime - lastAlert
+        if (diff.seconds) < self.delay:
+            self.logger.debug("Motion delay has not expired. %s seconds remaining." % diff.seconds)                
+        else:
+            lastAlert = curDTime
+            self.logger.debug("Motion detected at %s and motion delay has expired" % curDTime)
+            
+            # Check listen loop to ensure brain is not busy with an event
+            if self.ENVIRON["listen"] == True:
+                command = None
+                # if in security camera mode then capture video
+                #----------------------------------------------
+                if self.ENVIRON["security"] == True:
+                    self.logger.debug("Security mode is enabled so trigger security alert")
+                    self.securityWarn(camera)
+                # else check whether we should begin a chat loop
+                #----------------------------------------------
+                else:
+                    self.logger.debug("Checking if we should trigger a chat")
+                    diff = 0
+                    # only trigger a chat if we have a delay and a chat ID
+                    if self.chatDelay > 0 and len(self.motionChat) > 0:
+                        diff = curDTime - self.lastChat
+                    if diff > datetime.timedelta(minutes=self.chatDelay):
+                        self.lastChat = curDTime
+                        command = 'CHATBOT:%s' % self.motionChat
+                        self.ENVIRON["listen"] = False
+                        self.logger.debug("Posting %s to Queue" % command)
+                        self.SENSORQ.put(['brain', command])
+        return lastAlert
+        
+        
+    # Loop to detect motion using the PIR Sensor
     # ============================================================================================
     def detectHuman(self):
-        self.logger.debug("Using PIR sensor on pin %s to detect motion" % self.PIRPin)
+        self.logger.debug("Using PIR sensor on pin %s to detect motion" % self.detectPin)
         import RPi.GPIO as GPIO
-        import time
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.detectPin, GPIO.IN, GPIO.PUD_UP)
         camera = cv2.VideoCapture(0)
+        lastAlert = datetime.datetime.today()
+        frames = 0
+        
+        # loop continuously cnd check GPIO Pin for motion
         while True:
-            if GPIO.input(pirPin) == GPIO.LOW:
-                print ("No Motion")
+            if GPIO.input(self.detectPin) == GPIO.LOW:
+                self.logger.debug("No motion detected yet.")
             else:
-                curDTime = datetime.datetime.today()
-                self.logger.debug("Motion detected at %s " % curDTime)
-                diff = curDTime - lastAlert
-                if (diff.seconds) < self.delay:
-                    self.logger.debug("Motion delay of %s seconds has not expired" % curDTime)                
-                else:
-                    lastAlert = curDTime
-                    self.logger.debug("Motion detected at %s and motion delay has expired" % curDTime)
-                    # Need to make sure that listen loop does not clash with motion alert
-                    if self.ENVIRON["listen"] == True:
-                        command = None
-                        # if in security camera mode then fire the alarm
-                        if self.ENVIRON["security"] == True:
-                            self.logger.debug("Security mode is enabled so trigger security alert")
-                            self.securityWarn(camera)
-                        # else check whether we should begin a chat loop
-                        else:
-                            self.logger.debug("Checking if we should trigger a chat")
-                            diff = 0
-                            # only trigger a chat if we have a delay and a chat ID
-                            if self.chatDelay > 0 and len(self.motionChat) > 0:
-                                diff = curDTime - self.lastChat
-                            if diff > datetime.timedelta(minutes=self.chatDelay):
-                                self.lastChat = curDTime
-                                command = 'CHATBOT:%s' % self.motionChat
-                                self.ENVIRON["listen"] = False
-                                self.logger.debug("Posting %s to Queue" % command)
-                                self.SENSORQ.put(['brain', command])
-    
+                print ("Last Alert BEFORE detection event is %s" % lastAlert)
+                lastAlert = self.detectionEvent(lastAlert, camera)
+                print ("Last Alert AFTER detection event is %s" % lastAlert)
 
-    
-    # Loop to actually detect motion using the camera
+            frames += 1
+            # check the ENVIRON when frame count reaches check point
+            if frames > self.framesCheck:
+                self.logger.debug("Checking to see if we should stop detecting motion")
+                frames = 0
+                if not self.ENVIRON["motion"]:
+                    self.logger.debug("Time to stop detecting motion")
+                    # cleanup the camera quit function
+                    camera.release()
+                    break
+                    
+            #pause before next loop
+            time.sleep(0.3)
+
+            
+    # Loop to detect motion using the camera
     # ============================================================================================
     def detectMotion(self):
         self.logger.debug("Starting to detect Motion")
@@ -244,10 +273,8 @@ class motionLoop(object):
             # compute the absolute difference between the current frame and first frame
             frameDelta = cv2.absdiff(firstFrame, gray)
             thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-
             # Update the reference frame
             firstFrame = gray
-
             # dilate the thresholded image to fill in holes, then find contours on thresholded image
             thresh = cv2.dilate(thresh, None, iterations=2)
             (image, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -257,43 +284,10 @@ class motionLoop(object):
                 # if the contour is too small, ignore it
                 if cv2.contourArea(c) < self.min_area:
                     continue
-
-                # Check lastAlert to see if we need to trigger a new Alert for the motion
-                #--------------------------------------------------------------------------
-                curDTime = datetime.datetime.today()
-                self.logger.debug("Motion detected at %s " % curDTime)
-                diff = curDTime - lastAlert
-                if (diff.seconds) < self.delay:
-                    self.logger.debug("Motion delay of %s seconds has not expired" % curDTime)                
-                else:
-                    lastAlert = curDTime
-                    self.logger.debug("Motion detected at %s and motion delay has expired" % curDTime)
-                    # Need to make sure that listen loop does not clash with motion alert
-                    if self.ENVIRON["listen"] == True:
-                        command = None
-                        
-                        # if in security camera mode then take pictures (now edited to capture video)
-                        #----------------------------------------------
-                        if self.ENVIRON["security"] == True:
-                            self.logger.debug("Security mode is enabled so trigger security alert")
-                            self.securityWarn(camera)
-                        # else check whether we should begin a chat loop
-                        #----------------------------------------------
-                        else:
-                            self.logger.debug("Checking if we should trigger a chat")
-                            diff = 0
-                            # only trigger a chat if we have a delay and a chat ID
-                            if self.chatDelay > 0 and len(self.motionChat) > 0:
-                                diff = curDTime - self.lastChat
-                            if diff > datetime.timedelta(minutes=self.chatDelay):
-                                self.lastChat = curDTime
-                                command = 'CHATBOT:%s' % self.motionChat
-                                self.ENVIRON["listen"] = False
-                                self.logger.debug("Posting %s to Queue" % command)
-                                self.SENSORQ.put(['brain', command])
+                #motion must be detected, see if we need to trigger a new Alert for the motion
+                lastAlert = self.detectionEvent(lastAlert, camera)
 
             # check the ENVIRON when frame count reaches check point
-            #-------------------------------------------------------
             if frames > self.framesCheck:
                 self.logger.debug("Checking to see if we should stop detecting motion")
                 frames = 0
@@ -324,4 +318,3 @@ if __name__ == "__main__":
     MIC = testSensor.createMic(ENVIRON, 'pico-tts')
     
     doSensor(ENVIRON, SENSORQ, MIC)
-
